f59c0e37487346457da7eb517af7fb1dffd4bd33
sradms0 <sradms0@openmailbox.org>
2020-01-14 14:44:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Show Attackers
+
+## Description
+Display IP addresses of excessive failed login attempts in a GNU/Linux system.
+
+## Run
+`./show-attackers.sh [FILE]`
---
+++ show-attackers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script displays IP addresses of excessive failed login attempts in a GNU/Linux system.
+
+# Make sure a file was supplied as an argument.
+
+# Display the CSV header.
+
+# Loop through the list of failed attempts and corresponding IP addresses.
+
+# If the number of failed attempts is greater than the limit, display
