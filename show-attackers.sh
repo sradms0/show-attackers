d6d3933d38b9d0a1ebaa64ebc00e6627d7a412f3
sradms0 <sradms0@openmailbox.org>
2020-01-14 15:04:25 -0500

    check for file argument and if that file exists

--- show-attackers.sh
+++ show-attackers.sh
@@ -2,7 +2,21 @@
 
 # This script displays IP addresses of excessive failed login attempts in a GNU/Linux system.
 
+LOG_FILE="${1}"
+
 # Make sure a file was supplied as an argument.
+if [[ "${#}" -ne 1 ]]
+then
+    echo "Usage: ${0} FILE" >&2
+    echo 'Display failed login attempts by IP address.'
+    exit 1
+fi
+
+if [[ ! -e "${LOG_FILE}" ]]
+then
+    echo "${LOG_FILE} not found" >&2
+    exit 1
+fi
 
 # Display the CSV header.
 
