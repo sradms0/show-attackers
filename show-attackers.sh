1e11e432a6a65f26e1e9db624d2bd4f4ead0ab07
sradms0 <sradms0@openmailbox.org>
2020-01-15 11:31:53 -0500

    add threshold var and failed attempt output

--- show-attackers.sh
+++ show-attackers.sh
@@ -3,6 +3,7 @@
 # This script displays IP addresses of excessive failed login attempts in a GNU/Linux system.
 
 LOG_FILE="${1}"
+ATTEMPT_THRESHOLD='10'
 
 # Make sure a file was supplied as an argument.
 if [[ "${#}" -ne 1 ]]
@@ -18,8 +19,16 @@ then
     exit 1
 fi
 
-# Display the CSV header.
-
-# Loop through the list of failed attempts and corresponding IP addresses.
-
-# If the number of failed attempts is greater than the limit, display
+# Display the CSV header, and failed attempts exceeding threshold
+echo 'Count,IP,Location'
+grep 'Failed' "${LOG_FILE}" | awk '{print $(NF - 3)}' | sort -n | uniq -c | sort -nr |
+while read LINE
+do
+    COUNT=$(echo "${LINE}" | awk '{print $1}')
+    IP=$(echo "${LINE}" | awk '{print $2}')
+    if [[ "${COUNT}" -gt "${ATTEMPT_THRESHOLD}" ]]
+    then
+        COUNTRY=$(geoiplookup "${IP}" | awk -F ', ' '{print $NF}')
+        echo "${COUNT},${IP},${COUNTRY}"
+    fi
+done 
